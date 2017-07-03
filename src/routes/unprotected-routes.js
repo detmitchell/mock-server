1d4c667e9e815fd31c7f7a38ba04acd5512d9557
Jeffrey Mitchell <detmitchell@gmail.com>
2017-07-03 08:58:31 -0500

    update route

--- src/routes/unprotected-routes.js
+++ src/routes/unprotected-routes.js
@@ -11,11 +11,12 @@ app.get('/api/random-quote', function(req, res){
   res.status(200).send(quoter.getRandomQuote());
 });
 
-app.get('/api/product-search', function(req, res){
+app.post('/api/product-search', function(req, res){
+  console.log('product search requested for paramaters:'+req.body)
   if(!req.body || !req.body.keySearch){
     res.status(400).send('Please enter some keywords to search');
   }
   searcher.search(req.body.keySearch, function(err, resp){
     res.status(err ? 400 : 200).send(err ? err : resp);
-  })
+  });
 })
